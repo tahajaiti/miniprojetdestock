d9252aecfc09adf234965bd42de8a464c298a381
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-27 19:10:37 +0100

    projet

--- gestiondestock.c
+++ gestiondestock.c
@@ -25,7 +25,7 @@ int main() {
     int total = 0;
     int choice;
     char search[100];
-    int i,new;
+    int i,j,new;
 
     do{
         printf(BBLU"\n***** MENU *****\n");
@@ -42,33 +42,30 @@ int main() {
             printf(BRED"\nEntrée invalide.\n");
             sleep(1);
             while (getchar() != '\n');
-            continue;}
-            
+            continue;
+        }
         getchar();
 
         switch (choice){
             case 1: //ajouter un stock
                 if (total < MAX){
-                    getchar();
 
                     printf(BYEL"\ntitre de livre: ");
-                    fgets(titre[total], 100, stdin);
+                    fgets(titre[total], sizeof(titre[total]), stdin);
                     titre[total][strcspn(titre[total], "\n")] = '\0';
                     
                     printf(BYEL"\nauteur de livre: ");
-                    fgets(auteur[total], 100, stdin);
+                    fgets(auteur[total], sizeof(auteur[total]), stdin);
                     auteur[total][strcspn(auteur[total], "\n")] = '\0';
 
                     printf(BYEL"\nprix de livre: ");
                     while (scanf("%f", &prix[total]) != 1) {
-                        printf(BRED"\nEntree invalide. entrer un prix valide: ");
-                        while (getchar() != '\n');
-                    }
+                        printf(BRED"\nEntree invalide." BYEL" entrer un prix valide: ");
+                        while (getchar() != '\n');}
                     printf(BYEL"\nquantite de livre: ");
                     while (scanf("%d", &quantite[total]) != 1) {
-                        printf(BRED"\nEntree invalide. entrer un quntite valide: ");
-                        while (getchar() != '\n');
-                    }
+                        printf(BRED"\nEntree invalide." BYEL" entrer un prix valide: ");
+                        while (getchar() != '\n');}
 
                     printf(BGRN"\nCette operation est un succes. \n");
 
@@ -84,7 +81,7 @@ int main() {
                     printf(BRED"il n y a pas de stock");} sleep(3); break;
             case 3: // search for stock
                 printf(BYEL"\nEntrez le titre du livre a rechercher: ");
-                fgets(search, 100, stdin);
+                fgets(search, sizeof(search), stdin);
                 search[strcspn(search, "\n")] = '\0';
 
                 for (i = 0; i < total; i++) {
@@ -99,9 +96,8 @@ int main() {
                     sleep(1);
                     break;}
             case 4: //update stock
-                printf(BYEL"Entrez le titre du livre a mettre a jour: ");
-                fgets(search, 100, stdin);
-                getchar();
+                printf(BYEL"Entrez le titre du livre à mettre a jour: ");
+                fgets(search, sizeof(search), stdin);
                 search[strcspn(search, "\n")] = '\0';
 
                 for (i = 0; i < total; i++) {
@@ -117,6 +113,24 @@ int main() {
                 if (i == total) {
                         printf(BRED"\nle livre nexiste pas.\n");
                         sleep(1);}break;
+            case 5:
+                printf(BYEL"Entrez le titre du livre a supprimer: ");
+                fgets(search, sizeof(search), stdin);
+                search[strcspn(search, "\n")] = '\0';
+
+                for (i = 0; i < total; i++) {
+                    if (strcmp(titre[i], search) == 0) {
+                        for (j = i; j < total - 1; j++) {
+                            strcpy(titre[j], titre[j + 1]);
+                            strcpy(auteur[j], auteur[j + 1]);
+                            prix[j] = prix[j + 1];
+                            quantite[j] = quantite[j + 1];
+                        }
+                        total--;
+                        printf(BRED"Livre supprime.\n");sleep(1);break;}}
+            if (i == total) {
+                printf(BRED"le livre nexiste pas.\n");sleep(1);}
+
         default:
             break;
         }
