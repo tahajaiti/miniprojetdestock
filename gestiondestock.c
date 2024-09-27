d9ff496af704c9356ab53380fb2c892c06552eb3
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-27 16:24:07 +0100

    projet

--- gestiondestock.c
+++ gestiondestock.c
@@ -8,13 +8,14 @@ int main() {
 
     const int MAX = 100;
 
-    char titre[MAX][50];
-    char auteur[MAX][50];
+    char titre[MAX][100];
+    char auteur[MAX][100];
     float prix[MAX];
     int quantite[MAX];
     int total = 0;
     int choice;
-    char search;
+    char search[100];
+    int i;
 
     do{
         printf("\n\033[1;34m ***** MENU ***** \033[0m\n");
@@ -27,7 +28,8 @@ int main() {
         printf("\033[1;31m7.Quitter.\n\033[0m");
         
         if (scanf("%d", &choice) != 1) {
-            printf("\n\033[1;31mEntrée invalide. Veuillez entrer un nombre.\033[0m\n");
+            printf("\n\033[1;31mEntree invalide. \033[0m\n");
+            sleep(1);
             while (getchar() != '\n');
             continue;
         }
@@ -36,17 +38,19 @@ int main() {
             case 1:
                 if (total < MAX){
                     printf("\ntitre de livre: ");
-                    scanf("%49s", &titre[total]);
+                    fgets(titre[total], 100, stdin);
+                    titre[total][strcspn(titre[total], "\n")] = '\0';
+                    
                     printf("\nauteur de livre: ");
                     scanf("%49s", &auteur[total]);
                     printf("\nprix de livre: ");
                     while (scanf("%f", &prix[total]) != 1) {
-                        printf("\n\033[1;31mEntree invalide. entrer un prix valide: \033[0m\n");
+                        printf("\n\033[1;31mEntree invalide. entrer un prix valide: \033[0m");
                         while (getchar() != '\n');
                     }
                     printf("\nquantite de livre: ");
                     while (scanf("%d", &quantite[total]) != 1) {
-                        printf("\n\033[1;31mEntree invalide. entrer un quntite valide: \033[0m\n");
+                        printf("\n\033[1;31mEntree invalide. entrer un quntite valide: \033[0m");
                         while (getchar() != '\n');
                     }
 
@@ -57,12 +61,24 @@ int main() {
                     printf("\n\033[1;31mLe stock maximum est atteint. \n\033[0m");sleep(2);}break;
             case 2:
                 for (int i = 0; i < total; i++){
-                    printf("\n%d.Titre: %s , Auteur: %s , Prix: %.2f, Quantite: %d\n", i+1,
+                    printf("\n\033[1;33m%d.Titre: %s , Auteur: %s , Prix: %.2f, Quantite: %d\n\033[0m", i+1,
                     titre[i], auteur[i], prix[i], quantite[i]);
                 }sleep(3); break;
             case 3:
                 printf("Entrez le titre du livre a rechercher: ");
                 scanf("%s", &titre);
+                
+                search[strcspn(search, "\n")] = '\0';
+
+                for (i = 0; i < total; i++) {
+                  if (strcmp(titre[i], search) == 0) {
+                     printf("Livre trouve - Titre: %s, Auteur: %s, Prix: %.2f, Quantite: %d\n", titre[i], auteur[i], prix[i], quantite[i]);
+                     break;
+                    }
+                }
+                if (i == total) {
+                    printf("le livre nexiste pas.\n");
+                }
 
         default:
             break;
