8e530f85731ed4476c0aac86f835fe7d058046ad
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-27 17:10:55 +0100

    projet

--- gestiondestock.c
+++ gestiondestock.c
@@ -15,7 +15,7 @@ int main() {
     int total = 0;
     int choice;
     char search[100];
-    int i;
+    int i,new;
 
     do{
         printf("\n\033[1;34m ***** MENU ***** \033[0m\n");
@@ -26,23 +26,27 @@ int main() {
         printf("5.Supprimer un livre du stock.\n");
         printf("6.Afficher le nombre total de livres en stock.\033[0m\n");
         printf("\033[1;31m7.Quitter.\n\033[0m");
+        printf("\033[1;33mEntrer un nombre: \033[0m");
         
         if (scanf("%d", &choice) != 1) {
-            printf("\n\033[1;31mEntree invalide. \033[0m\n");
+            printf("\n\033[1;31mEntrée invalide. \033[0m\n");
             sleep(1);
             while (getchar() != '\n');
-            continue;
-        }
+            continue;}
 
         switch (choice){
-            case 1:
+            case 1: //ajouter un stock
                 if (total < MAX){
+                    getchar();
+
                     printf("\ntitre de livre: ");
                     fgets(titre[total], 100, stdin);
                     titre[total][strcspn(titre[total], "\n")] = '\0';
                     
                     printf("\nauteur de livre: ");
-                    scanf("%49s", &auteur[total]);
+                    fgets(auteur[total], 100, stdin);
+                    auteur[total][strcspn(auteur[total], "\n")] = '\0';
+
                     printf("\nprix de livre: ");
                     while (scanf("%f", &prix[total]) != 1) {
                         printf("\n\033[1;31mEntree invalide. entrer un prix valide: \033[0m");
@@ -59,27 +63,47 @@ int main() {
                     total++;sleep(2);}
                 else {
                     printf("\n\033[1;31mLe stock maximum est atteint. \n\033[0m");sleep(2);}break;
-            case 2:
+            case 2: //afficher le stock
                 for (int i = 0; i < total; i++){
-                    printf("\n\033[1;33m%d.Titre: %s , Auteur: %s , Prix: %.2f, Quantite: %d\n\033[0m", i+1,
+                    printf("\n\033[1;33m%d.Titre: %s , Auteur: %s , Prix: %.2f DH, Quantite: %d\n\033[0m", i+1,
                     titre[i], auteur[i], prix[i], quantite[i]);
                 }sleep(3); break;
-            case 3:
-                printf("Entrez le titre du livre a rechercher: ");
-                scanf("%s", &titre);
-                
+            case 3: // search for stock
+                getchar();
+
+                printf("\nEntrez le titre du livre a rechercher: ");
+                fgets(search, 100, stdin);
                 search[strcspn(search, "\n")] = '\0';
 
                 for (i = 0; i < total; i++) {
                   if (strcmp(titre[i], search) == 0) {
-                     printf("Livre trouve - Titre: %s, Auteur: %s, Prix: %.2f, Quantite: %d\n", titre[i], auteur[i], prix[i], quantite[i]);
-                     break;
+                    printf("\n\033[1;33mLivre trouve - Titre: %s, Auteur: %s, Prix: %.2f, Quantite: %d\n\033[0m", titre[i], auteur[i], prix[i], quantite[i]);
+                    sleep(2);
+                    break;
                     }
                 }
                 if (i == total) {
-                    printf("le livre nexiste pas.\n");
-                }
+                    printf("\n\033[1;33mle livre nexiste pas.\n\033[0m");
+                    sleep(1);
+                    break;}
+            case 4: //update stock
+                printf("Entrez le titre du livre a mettre a jour: ");
+                fgets(search, 100, stdin);
+                search[strcspn(search, "\n")] = '\0';
 
+                for (i = 0; i < total; i++) {
+                    if (strcmp(titre[i], search) == 0) {
+                        printf("Entrez la nouvelle quantite: ");
+                        scanf("%d", &new);
+                        getchar();
+                        quantite[i] = new;
+                        printf("Quantite mise a jour.\n");
+                        break;
+                    }
+                }
+                if (i == total) {
+                        printf("\n\033[1;33mle livre nexiste pas.\n\033[0m");
+                        sleep(1);}break;
         default:
             break;
         }
